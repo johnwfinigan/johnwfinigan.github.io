44e0aa0c1b9e4dce286c594ab49fe3002098ae66
john <john@q.my.domain>
2022-09-12 07:49:39 -0500

    change index file format to be generic delimited text

--- gen.sh
+++ gen.sh
@@ -5,6 +5,7 @@ set -eu
 temp_index_md=$(mktemp)
 temp_html=$(mktemp)
 temp_index=$(mktemp)
+temp_index_sorted=$(mktemp)
 mkdir -p dst
 
 
@@ -26,10 +27,9 @@ for md in src/*.md ; do
 
 
   # create input for index generator
-  # index input is markdown format
   day=$(lowdown -X date "$md")
   title=$(lowdown -X title "$md")
-  printf "* %s: [%s](%s)\n" "$day" "$title" "${htm}" >> "$temp_index"
+  printf "%s^%s^%s\n" "$day" "$title" "${htm}" >> "$temp_index"
 
 done
 
@@ -44,7 +44,9 @@ css: simple.css
 HERE
 
 # sort index entries by date
-sort -r -k1 -t: "$temp_index" >> "$temp_index_md"
+sort -r -k1 -t^ "$temp_index" > "$temp_index_sorted"
+# create markdown from sorted index
+awk -F^ '{printf "* %s: [%s](%s)\n", $1, $2, $3}' < "$temp_index_sorted" >> "$temp_index_md"
 
 # make html index page
 lowdown -s "$temp_index_md" | \
@@ -54,6 +56,6 @@ cat "$temp_html" src/footer > "dst/index.html"
 
 
 
-rm -f "$temp_index_md" "$temp_index" "$temp_html"
+rm -f "$temp_index_md" "$temp_index" "$temp_index_sorted" "$temp_html"
 
 cp dst/*.html .
